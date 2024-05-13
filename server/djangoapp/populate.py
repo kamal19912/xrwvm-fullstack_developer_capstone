936d45546e9be96f36203d9c63ae98638b3082e3
kamal9912 <kamalpatidar091@gmail.com>
2024-05-13 05:50:50 -0400

    Adding temporary changes to Github

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -2,10 +2,6 @@ from .models import CarMake, CarModel
 
 
 def initiate():
-    print("Populate not implemented. Add data manually")
-
-
-def initiate():
     car_make_data = [
         {"name": "NISSAN", "description": "Great cars. Japanese technology"},
         {"name": "Mercedes", "description": "Great cars. German technology"},
@@ -37,4 +33,3 @@ def initiate():
     ]
     for data in car_model_data:
         CarModel.objects.create(name=data['name'], car_make=data['car_make'], type=data['type'], year=data['year'])
-        
