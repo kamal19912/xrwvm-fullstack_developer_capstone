49afe0a38a6d352e9d1ced82ab4b6374d898bea1
kamal9912 <kamalpatidar091@gmail.com>
2024-05-14 01:19:44 -0400

    Adding temporary changes to Github

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -27,9 +27,11 @@ SECRET_KEY = \
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost',
-                 'https://kamal2314106-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+                 'https://kamal2314106-8000.theiadockernext-1-labs-'
+                 'prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 CSRF_TRUSTED_ORIGINS = [
-    'https://kamal2314106-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+    'https://kamal2314106-8000.theiadockernext-1-'
+    'labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -93,7 +95,8 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-            'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+            'django.contrib.auth.password_validation.'
+            'UserAttributeSimilarityValidator',
     },
     {
         'NAME':
