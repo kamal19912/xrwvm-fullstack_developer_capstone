9d93fef8427bdaeec56713e8656fc0f6848a4182
kamal9912 <kamalpatidar091@gmail.com>
2024-05-14 05:46:01 -0400

    Adding temporary changes to Github

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,4 +1,3 @@
-/*eslint no-unused-vars: ["error", { "vars": "local" }]*/
 # Uncomment the required imports before adding the code
 
 # from django.shortcuts import render
@@ -126,7 +125,7 @@ def add_review(request):
     if (request.user.is_anonymous is False):
         data = json.loads(request.body)
         try:
-            response = post_review(data)
+            request = post_review(data)
             return JsonResponse({"status": 200})
         except Exception:
             return JsonResponse({"status": 401,
